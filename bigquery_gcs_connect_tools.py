90ef78ab579b80a58635f005a3d9ccb1834a4121
Leann Chen <80327331+leannchen86@users.noreply.github.com>
2023-04-10 11:34:01 -0500

    Add files via upload

---
+++ bigquery_gcs_connect_tools.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from google.cloud import storage, bigquery
+from io import StringIO
+import os
+# Set your credentials
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'D:/channel_analytics/service_account_key.json'
+bucket_name = "lkdatabase"
+# Create a storage client
+storage_client = storage.Client()
+bucket = storage_client.get_bucket(bucket_name) # Get the bucket object
+blobs = storage_client.list_blobs(bucket_name) # Get the blob object
+
+# Map pandas data types to BigQuery data types
+def pandas_type_to_bigquery_type(dtype):
+    if pd.api.types.is_integer_dtype(dtype):
+        return 'INT64'
+    elif pd.api.types.is_float_dtype(dtype):
+        return 'FLOAT64'
+    elif pd.api.types.is_bool_dtype(dtype):
+        return 'BOOL'
+    else:
+        return 'STRING'
+
+def get_files_from_gcs():
+    file_lst_gcs = []
+    # Print the file names
+    for blob in blobs:
+        file_lst_gcs.append(blob.name)
+    
+    return file_lst_gcs
+
+def get_schema(file_name):
+    
+    blob = storage.Blob(file_name, bucket)
+    # Read the CSV file content
+    csv_content = blob.download_as_text()
+    
+    # Convert the content to a pandas DataFrame
+    data_frame = pd.read_csv(StringIO(csv_content))
+    # Create the BigQuery schema
+    schema = [
+        bigquery.SchemaField(column_name, pandas_type_to_bigquery_type(dtype))
+        for column_name, dtype in zip(data_frame.columns, data_frame.dtypes)
+    ]
+    return schema
---
+++ bigquery_gcs_connection.py
@@ -0,0 +1,36 @@
+import os
+import cloud_storage as gcs
+from google.cloud import bigquery
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'D:/channel_analytics/service_account_key.json'
+client = bigquery.Client()
+dataset_id = 'lkdata'
+dataset_ref = bigquery.DatasetReference(client.project, dataset_id)
+
+table_id_list = gcs.get_files_from_gcs()
+
+table_id_list.remove('p_channel_demographics_a1')
+
+for i in range(len(table_id_list)):
+    table_originalId = table_id_list[i]
+    table_id = f"{table_originalId}_gcs"
+
+    schema = gcs.get_schema(table_id_list[i])
+    table_ref = bigquery.Table(dataset_ref.table(table_id), schema=schema)
+
+    external_config = bigquery.ExternalConfig('CSV')
+    source_uri = f"gs://lkdatabase/{table_originalId}"
+    external_config.source_uris = [source_uri]
+    external_config.csv_options.skip_leading_rows = 1
+
+    table_ref.external_data_configuration = external_config
+    table = client.create_table(table_ref)
+
+    sql = 'SELECT * FROM `{0}.{1}`'.format(dataset_id, table_id)
+
+    query_job = client.query(sql)
+
+    output = list(query_job)
+
+
+
